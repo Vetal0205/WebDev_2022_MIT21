3b32c446c1c8f664de0d257e8effe6a10b21c0a0
Vetal0205 <vetal0205merkulov@gmail.com>
2022-10-02 19:50:59 +0100

    "shadow" header on scroll

--- Labs/lab3/main.js
+++ Labs/lab3/main.js
@@ -3,3 +3,6 @@ let search_bar = document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () => {
     search_bar.classList.toggle('active');
 }
+
+let header = document.querySelector('header');
+window.addEventListener('scroll', () => header.classList.toggle('shadow', window.scrollY > 0))
